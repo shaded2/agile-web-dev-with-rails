d3ddc14fcba9bad985e706f78d3a7d1c1f1a273f
Darren Cato <darren.cato@oracle.com>
2012-11-07 12:30:02 -0500

    Created controller to handle customer centeric interactions as well as functional tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,8 +4,8 @@ class Product < ActiveRecord::Base
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
-  	with: %r{\.(gif|jpg|pnf)\Z}i,
-  	message: 'Must a url for gif, jpep or png image'
+  	with: %r{\.(gif|jpg|png)\Z}i,
+  	message: 'Must a url for gif, jpeg or png image'
   }
 
 end
